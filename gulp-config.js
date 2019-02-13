658ab897202f0fc4d61d1de2b545cb7f8b36220b
SitecoreCpok <37365770+SitecoreCpok@users.noreply.github.com>
2019-02-13 14:54:03 +0530

    Initial Commit

---
+++ gulp-config.js
@@ -0,0 +1,10 @@
+module.exports = function () {
+    var config = {
+        webRoot: "c:\\inetpub\\wwwroot\\yourinstancename",
+        devRoot: "c:\\projects\\yourdevroot",
+        solutionName: "SitecoreTraininf",
+        buildConfiguration: "Debug",
+        MSBuildToolsVersion: 'auto'
+    }
+    return config;
+}
